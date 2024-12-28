e46e332b0230829ae4b098647afd80ffbca17f60
Chirag Parmar <chiragp@mechasystems.com>
2024-12-28 15:16:05 +0530

    board: mecha: fix: remove logs of brcmf_set_channel called by brcmf_cfg80211_dump_survey function
    Signed-off-by: Chirag Parmar <chiragp@mechasystems.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/feature.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/feature.c
@@ -329,7 +329,7 @@ void brcmf_feat_attach(struct brcmf_pub *drvr)
 	brcmf_feat_iovar_int_get(ifp, BRCMF_FEAT_RSDB, "rsdb_mode");
 	brcmf_feat_iovar_int_get(ifp, BRCMF_FEAT_TDLS, "tdls_enable");
 	brcmf_feat_iovar_int_get(ifp, BRCMF_FEAT_MFP, "mfp");
-	brcmf_feat_iovar_int_get(ifp, BRCMF_FEAT_DUMP_OBSS, "dump_obss");
+//	brcmf_feat_iovar_int_get(ifp, BRCMF_FEAT_DUMP_OBSS, "dump_obss");
 
 	pfn_mac.version = BRCMF_PFN_MACADDR_CFG_VER;
 	err = brcmf_fil_iovar_data_get(ifp, "pfn_macaddr", &pfn_mac,
